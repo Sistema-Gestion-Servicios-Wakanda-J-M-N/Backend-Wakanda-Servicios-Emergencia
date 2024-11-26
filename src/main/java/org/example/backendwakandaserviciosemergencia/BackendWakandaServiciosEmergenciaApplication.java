ba9242269846ecaf8b388813549123e99e7f9cb7
marcos <marcgabe@myuax.com>
2024-11-26 13:36:01 +0100

    Arquitectura V 1.0
    Inicial Commit

---
+++ src/main/java/org/example/backendwakandaserviciosemergencia/BackendWakandaServiciosEmergenciaApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaserviciosemergencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaServiciosEmergenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWakandaServiciosEmergenciaApplication.class, args);
+    }
+
+}
