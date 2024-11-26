fe9ef60e8913e2b96d8c82ef5d01557c9e405b2f
marcos <marcgabe@myuax.com>
2024-11-26 16:38:46 +0100

    CONFIGURACIÓN (paquete config)
    Clavamos aquí de misma forma la configuración base reutilizable en principio para todos los microservicios que desarrollamos.
    
    Desde el cuartel general nos los traemos

---
+++ src/main/java/org/example/backendwakandaserviciosemergencia/config/DomainConfig.java
@@ -0,0 +1,26 @@
+package org.example.backendwakandaserviciosemergencia.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+@Configuration
+@EntityScan("org.example.backendwakandaapicentral.domain")
+@EnableJpaRepositories("org.example.backendwakandaserviciosemergencia.repos")
+@EnableTransactionManagement
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+public class DomainConfig {
+
+    @Bean(name = "auditingDateTimeProvider")
+    public DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
+    }
+}
+
